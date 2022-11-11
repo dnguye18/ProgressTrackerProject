ab0c058f2c5e7e5d42868ee324b517267334c2d2
Rocketblast1 <ganumba1@gmail.com>
2022-11-11 01:42:06 -0800

    Added DAO

--- ProgressTrackerProject/src/main/java/UserDAO.java
+++ ProgressTrackerProject/src/main/java/UserDAO.java
@@ -1,5 +1,8 @@
 package main.java;
 
-public class UserDAO {
-
+public interface  UserDAO {
+	public boolean addUser(User u);
+	public User getUser(int u_id);
+	public boolean deleteUser(int u_id);
+	public boolean updateUser(User u);
 }
--- ProgressTrackerProject/src/main/java/WatchlistDAO.java
+++ ProgressTrackerProject/src/main/java/WatchlistDAO.java
@@ -1,5 +1,8 @@
 package main.java;
 
 public interface WatchlistDAO {
-
+	public boolean addWatchlist(Watchlist w);
+	public Watchlist getWatchlist(int w_id);
+	public boolean deleteWatchlist(int w_id);
+	public boolean updateWatchlist(Watchlist w);
 }
